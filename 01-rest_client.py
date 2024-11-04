75523db4140ad4bf7269e4b3c2261847dc78bc7c
Amishasingh112003 <151494401+Amishasingh112003@users.noreply.github.com>
2024-11-04 19:23:01 +0530

    Update 01-rest_client.py

--- 01-rest_client.py
+++ 01-rest_client.py
@@ -1,33 +1,84 @@
-#here is the rest_client environment
-AI_SERVICE_ENDPOINT='https://azuresneha.cognitiveservices.azure.com/'
-AI_SERVICE_KEY='FZOgThN0b4kRH6Bc0J9EZw2t0WY7t8cVLkEpYhKXtZA2DZCAzxz1JQQJ99AKACYeBjFXJ3w3AAAAACOGe0Si'
-#here is the rest_client
-from dotenv
-import load_dotenv
-import osfrom azure.core.credentials 
-import AzureKeyCredentialfrom azure.ai.textanalytics
-import TextAnalyticsClient 
-def main():  
-  global ai_endpoint 
-  global ai_key   
-try:      
-  # Get Configuration Settings 
-  load_dotenv()   
-  ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')   
-  ai_key = os.getenv('AI_SERVICE_KEY')   
-  # Get user input (until they enter "quit")  
-userText ='' 
-while userText.lower() != 'quit':    
-  userText = input('\nEnter some text ("quit" to stop)\n')  
-  if userText.lower() != 'quit':     
-    language = GetLanguage(userText)   
-    print('Language:', language) 
-except Exception as ex:  
-print(ex)def GetLanguage(text): 
-# Create client using endpoint and key
-  credential = AzureKeyCredential(ai_key)
-  client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)  
-  # Call the service to get the detected language
-detectedLanguage = client.detect_language(documents = [text])[0]  
-return detectedLanguage.primary_language.nameif _name_ == "_main_": 
-main()
+"""We need to set an environment variable .This must have :
+  * endpoint of the azure as : "AI_SERVICE_ENDPOINT"
+  * API key as : "AI_SERVICE_KEY"
+  * AI_SERVICE_ENDPOINT='https://xxxxxazu.cognitivervices.azure.com/'
+  * AI_SERVICE_KEY='DcccvvvbbxxxEFnNrPjRy3o0urZ3xxxx73d8prTm4oxrXl78Bxxxxx9AKACYcccccJ3ccAACOGqFUd' 
+"""
+#this is the rest_client.py file
+
+from dotenv import load_dotenv
+import os
+import http.client
+import json
+
+def main():
+    global ai_endpoint
+    global ai_key
+    try:
+        # Get Configuration Settings
+        load_dotenv()
+        ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')
+        ai_key = os.getenv('AI_SERVICE_KEY')
+
+        # Get user input (until they enter "quit")
+        userText = ''
+        while userText.lower() != 'quit':
+            userText = input('Enter some text ("quit" to stop)\n')
+            if userText.lower() != 'quit':
+                GetLanguage(userText)
+    except Exception as ex:
+        print(ex)
+
+def GetLanguage(text):
+    try:
+        # Construct the JSON request body (a collection of documents, each with an ID and text)
+        jsonBody = {
+            "documents": [
+                {
+                    "id": 1,
+                    "text": text
+                }
+            ]
+        }
+
+        # Let's take a look at the JSON we'll send to the service
+        print(json.dumps(jsonBody, indent=2))
+
+        # Make an HTTP request to the REST interface
+        uri = ai_endpoint.rstrip('/').replace('https://', '')
+        conn = http.client.HTTPSConnection(uri)
+
+        # Add the authentication key to the request header
+        headers = {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': ai_key
+        }
+
+        # Use the Text Analytics language API
+        conn.request("POST", "/text/analytics/v3.1/languages?", str(jsonBody).encode('utf-8'), headers)
+
+        # Send the request
+        response = conn.getresponse()
+        data = response.read().decode("UTF-8")
+
+        # If the call was successful, get the response
+        if response.status == 200:
+            # Display the JSON response in full (just so we can see it)
+            results = json.loads(data)
+            print(json.dumps(results, indent=2))
+
+            # Extract the detected language name for each document
+            for document in results["documents"]:
+                print("\nLanguage:", document["detectedLanguage"]["name"])
+        else:
+            # Something went wrong, write the whole response
+            print(data)
+
+        conn.close()
+    except Exception as ex:
+        print(ex)
+
+if __name__ == "__main__":
+    main()
+
+
